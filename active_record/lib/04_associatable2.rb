927da4c59ef8957d6e5a2a8a49c782e71d7c1da7
dwebster17 <dwebster1992@gmail.com>
2017-09-22 18:46:53 -0400

    Commited associatable extension and #has_one_through method

--- active_record/lib/04_associatable2.rb
+++ active_record/lib/04_associatable2.rb
@@ -9,13 +9,19 @@ module Associatable
 
     define_method(name) do
       source_options = through_options.model_class.assoc_options[source_name]
-      DBConnection.execute(<<-SQL)
+
+      attr_hash = DBConnection.execute(<<-SQL, self.id)
         SELECT
-          #{through_options.model_class.table_name}.*
+          #{source_options.table_name}.*
         FROM
-          #{through_options.model_class.table_name}
-      hash = through_options..parse_all(source_options)
-      through_options.model_class.new(hash)
+          #{through_options.table_name}
+          JOIN #{source_options.table_name}
+          ON #{source_options.foreign_key} = #{source_options.table_name}.id
+        WHERE
+          #{source_options.table_name}.id = ?
+      SQL
+
+      source_options.model_class.new(attr_hash.first)
     end
   end
 end
